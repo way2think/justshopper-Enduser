2204d76eef1944ab01fe03bbeb559fbcfb9a6856
JagadishBujji <jagadish00198@gmail.com>
2024-03-15 16:23:49 +0530

    cartpage completed

--- src/component/CartComponent/CartTable.js
+++ src/component/CartComponent/CartTable.js
@@ -1,8 +1,21 @@
 import React from "react";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
 import "./CartTable.css";
-import { Button } from "@mui/material";
+import { Box, Button, Stack } from "@mui/material";
 
 const CartTable = () => {
+  const checkout = {
+    background: "#dc3237",
+    color: "#fff",
+    fontSize: "18px",
+    "&:hover": {
+      border: "2px solid #dc3237",
+      background: "transparent",
+      color: "#dc3237",
+      fontSize: "18px",
+    },
+  };
   return (
     <div class="container mt-5">
       <table class="table table-xs">
@@ -26,50 +39,75 @@ const CartTable = () => {
             <p className="itemdesc">STATIONERY - Notes</p>
           </td>
           <td className="text-right" title="Amount">
-            <p className="">- 1 +</p>
+            <Stack
+              direction="row"
+              justifyContent="space-around"
+              alignItems="center"
+              className="cartqty"
+            >
+              <RemoveIcon htmlColor="#dc3237" sx={{ cursor: "pointer" }} />
+              <p className="qty">1</p>
+              <AddIcon htmlColor="#dc3237" sx={{ cursor: "pointer" }} />
+            </Stack>
           </td>
-          <td className="text-right" title="Price">
-            2.00
+          <td className="text-right price" title="Price">
+            499.00
           </td>
-          <td className="text-right" title="Total">
-            6.00
+          <td className="text-right price" title="Total">
+            499.00
           </td>
         </tr>
         <tr className="item-row item-row-last">
           <td>
             {" "}
-            <img src="../images/chocolate.jpg" alt="" className="imagecion" />
+            <img src="../images/biscuit.jpg" alt="" className="imagecion" />
           </td>
           <td className="Items">
             <p className="itemname">
               {" "}
-              <strong>Choco Bar</strong>
+              <strong>Choco Biscuit</strong>
             </p>
             <p className="itemdesc">STATIONERY - Notes</p>
           </td>
           <td className="text-right" title="Amount">
-            3
+            <Stack
+              direction="row"
+              justifyContent="space-around"
+              alignItems="center"
+              className="cartqty"
+            >
+              <RemoveIcon htmlColor="#dc3237" sx={{ cursor: "pointer" }} />
+              <p className="qty">1</p>
+              <AddIcon htmlColor="#dc3237" sx={{ cursor: "pointer" }} />
+            </Stack>
           </td>
-          <td className="text-right" title="Price">
-            4.00
+          <td className="text-right price" title="Price">
+            499.00
           </td>
-          <td className="text-right" title="Total">
-            12.00
+          <td className="text-right price" title="Total">
+            499.00
           </td>
         </tr>
         <tr className="total-row info">
-          <td className="text-right" colspan="4">
+          <td className="text-right price" colspan="4">
             Sub Total
           </td>
-          <td className="text-right">18.00</td>
+          <td className="text-right price">898.00</td>
         </tr>
         <tr className="total-row info">
-          <td className="text-right" colspan="4">
+          <td className="text-right price" colspan="4">
             Total
           </td>
-          <td className="text-right">18.00</td>
+          <td className="text-right price">898.00</td>
         </tr>
       </table>
+      <Stack direction="row" justifyContent="end" alignItems="center">
+        {/* <td className="text-right price">Checkout</td> */}
+        {/* <td className="text-right price">898.00</td> */}
+        <Button sx={checkout} className="checkoutbtn" variant="contained">
+          Checkout
+        </Button>
+      </Stack>
     </div>
   );
 };
