3eabdd470640c8ddd16dff8e2f0704f897235332
Rajeswari21300 <rajeswari21300@gmail.com>
2024-03-22 18:35:34 +0530

    card img,space

--- src/component/CartComponent/CartTable.js
+++ src/component/CartComponent/CartTable.js
@@ -1,6 +1,7 @@
 import React from "react";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
+import DeleteIcon from "@mui/icons-material/Delete";
 import "./CartTable.css";
 import { Box, Button, Stack } from "@mui/material";
 
@@ -14,6 +15,18 @@ const CartTable = () => {
       background: "transparent",
       color: "#dc3237",
       fontSize: "18px",
+    },
+  };
+
+  const deleteicon = {
+    padding: "8px 15px",
+    marginLeft: 2,
+    bgcolor: "#000",
+    fontWeight: 600,
+    "&:hover": {
+      border: "2px solid #000",
+      bgcolor: "#fff",
+      color: "#000",
     },
   };
   return (
@@ -31,32 +44,60 @@ const CartTable = () => {
             {" "}
             <img src="../images/chocolate.jpg" alt="" className="imagecion" />
           </td> */}
-          <td className="Items">
-            <p className="itemname">
-              {" "}
-              <strong>Choco Bar</strong>
-            </p>
-            <p className="itemdesc">STATIONERY - Notes</p>
-          </td>
-          <td className="text-right" title="Amount">
-            <Stack
-              direction="row"
-              justifyContent="space-around"
-              alignItems="center"
-              className="cartqty"
-              sx={{
-                width: "50%",
-                "@media only screen and (min-width: 320px) and (max-width: 600px)":
-                  {
-                    width: "100%",
-                  },
+          <td
+            className="Items"
+            style={{ display: "flex", flexDirection: "row", padding: "4px" }}
+          >
+            <img
+              src="../images/biscuit.jpg"
+              alt=""
+              style={{ maxWidth: "180px", borderRadius: 30 }}
+            />
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                textAlign: "left",
               }}
             >
-              <RemoveIcon htmlColor="#dc3237" sx={{ cursor: "pointer" }} />
-              <p className="qty">1</p>
-              <AddIcon htmlColor="#dc3237" sx={{ cursor: "pointer" }} />
-            </Stack>
+              <p className="itemname">
+                {" "}
+                <strong>Choco Bar</strong>
+              </p>
+              <p className="itemdesc">STATIONERY - Notes</p>
+            </div>
           </td>
+          <td className="text-right" title="Amount">
+            <Box style={{ position: "relative" }}>
+            <div  className="qtyFlex" >
+              <Stack
+                direction="row"
+                justifyContent="space-around"
+                alignItems="center"
+                className="cartqty"
+                sx={{
+                  width: "50%",
+                  // "@media only screen and (min-width: 320px) and (max-width: 600px)":
+                  //   {
+                  //     width: "100%",
+                  //   },
+                }}
+              >
+                <RemoveIcon htmlColor="#dc3237" sx={{ cursor: "pointer" }} />
+                <p className="qty">1</p>
+                <AddIcon htmlColor="#dc3237" sx={{ cursor: "pointer" }} />
+              </Stack>
+              <Button
+                sx={deleteicon}
+                variant="contained"
+               className="delete"
+              >
+                <DeleteIcon />
+              </Button>
+              </div>
+            </Box>
+          </td>
+
           <td className="text-right price" title="Price">
             499.00
           </td>
@@ -69,31 +110,56 @@ const CartTable = () => {
             {" "}
             <img src="../images/biscuit.jpg" alt="" className="imagecion" />
           </td> */}
-          <td className="Items">
-            <p className="itemname">
-              {" "}
-              <strong>Choco Biscuit</strong>
-            </p>
-            <p className="itemdesc">STATIONERY - Notes</p>
-          </td>
-          <td className="text-right" title="Amount">
-            <Stack
-              direction="row"
-              justifyContent="space-around"
-              alignItems="center"
-              className="cartqty"
-              sx={{
-                width: "50%",
-                "@media only screen and (min-width: 320px) and (max-width: 600px)":
-                  {
-                    width: "100%",
-                  },
+          <td
+            className="Items"
+            style={{ display: "flex", flexDirection: "row", padding: "4px" }}
+          >
+            <img
+              src="../images/biscuit.jpg"
+              alt=""
+              style={{ maxWidth: "180px", borderRadius: 30 }}
+            />
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                textAlign: "left",
               }}
             >
-              <RemoveIcon htmlColor="#dc3237" sx={{ cursor: "pointer" }} />
-              <p className="qty">1</p>
-              <AddIcon htmlColor="#dc3237" sx={{ cursor: "pointer" }} />
-            </Stack>
+              <p className="itemname">
+                {" "}
+                <strong>Choco Bar</strong>
+              </p>
+              <p className="itemdesc">STATIONERY - Notes</p>
+            </div>
+          </td>
+          <td className="text-right" title="Amount">
+            <Box style={{ position: "relative" }}>
+              <Stack
+                direction="row"
+                justifyContent="space-around"
+                alignItems="center"
+                className="cartqty"
+                sx={{
+                  width: "50%",
+                  // "@media only screen and (min-width: 320px) and (max-width: 600px)":
+                  //   {
+                  //     width: "100%",
+                  //   },
+                }}
+              >
+                <RemoveIcon htmlColor="#dc3237" sx={{ cursor: "pointer" }} />
+                <p className="qty">1</p>
+                <AddIcon htmlColor="#dc3237" sx={{ cursor: "pointer" }} />
+              </Stack>
+              <Button
+                sx={deleteicon}
+                variant="contained"
+              className="delete"
+              >
+                <DeleteIcon />
+              </Button>
+            </Box>
           </td>
           <td className="text-right price" title="Price">
             499.00
