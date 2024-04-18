0be05730ffc5614445e46a3b67b31597e00e0d86
Rajeswari21300 <rajeswari21300@gmail.com>
2024-04-18 20:59:51 +0530

    homebanner map

--- src/component/MainBannerCarosuel.js
+++ src/component/MainBannerCarosuel.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   MDBBtn,
   MDBCarousel,
@@ -6,28 +6,70 @@ import {
   MDBCarouselCaption,
 } from "mdb-react-ui-kit";
 import "./MainBannerCarosuel.css";
+import { useGetSettingsQuery } from "../api/api";
 
 export default function MainBannerCarosuel() {
+  const { data } = useGetSettingsQuery();
+  const [activeItem, setActiveItem] = useState(0);
+  const homeBanner = data?.home_banner || [];
+  console.log("data for banner", homeBanner);
+
+  const handleNext = () => {
+    setActiveItem((prevItem) =>
+      prevItem === homeBanner.length - 1 ? 0 : prevItem + 1
+    );
+  };
+
+  const handlePrev = () => {
+    setActiveItem((prevItem) =>
+      prevItem === 0 ? homeBanner.length - 1 : prevItem - 1
+    );
+  };
+
   return (
     <MDBCarousel showIndicators showControls fade>
-      <MDBCarouselItem itemId={1} className="overall">
-        <img
-          src="../images/testimonial.png"
-          className="d-block w-100 carousel-img"
-          alt="..."
-        />
-        <div className="mask" style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}>
-          <MDBCarouselCaption className="caption">
-            <h1 className="slidercaption">
-              As men get older,
-              <br /> the toys get more expensive.
-            </h1>
-            <MDBBtn className="sliderbtn">Shop Now</MDBBtn>
-          </MDBCarouselCaption>
-        </div>
-      </MDBCarouselItem>
+      {homeBanner.map((banner, index) => (
+        <MDBCarouselItem
+          key={index}
+          itemId={index}
+          className="overall"
+          activeItem={activeItem}
+        >
+          <img
+            src={banner.images[0].url}
+            className="d-block w-100 carousel-img"
+            alt="..."
+          />
+          {/* <div
+            className="mask"
+            style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
+          >
+            <MDBCarouselCaption className="caption">
+              <h1 className="slidercaption">{banner.description}</h1>
+              <MDBBtn className="sliderbtn">Shop Now</MDBBtn>
+            </MDBCarouselCaption>
+          </div> */}
+        </MDBCarouselItem>
+      ))}
+      <MDBBtn
+        onClick={handlePrev}
+        className="carousel-control-prev"
+        aria-label="Previous"
+      >
+        <span aria-hidden="true">&laquo;</span>
+      </MDBBtn>
+      <MDBBtn
+        onClick={handleNext}
+        className="carousel-control-next"
+        aria-label="Next"
+      >
+        <span aria-hidden="true">&raquo;</span>
+      </MDBBtn>
+    </MDBCarousel>
+  );
 
-      <MDBCarouselItem itemId={2} className="overall">
+  {
+    /* <MDBCarouselItem itemId={2} className="overall">
         <img
           src="../images/Gift and toys.png"
           className="d-block w-100 carousel-img"
@@ -42,9 +84,11 @@ export default function MainBannerCarosuel() {
             <MDBBtn className="sliderbtn">Shop Now</MDBBtn>
           </MDBCarouselCaption>
         </div>
-      </MDBCarouselItem>
+      </MDBCarouselItem> */
+  }
 
-      <MDBCarouselItem itemId={3} className="overall">
+  {
+    /* <MDBCarouselItem itemId={3} className="overall">
         <img
           src="../images/Gift and toys.png"
           className=" w-100 carousel-img"
@@ -59,7 +103,6 @@ export default function MainBannerCarosuel() {
             <MDBBtn className="sliderbtn">Shop Now</MDBBtn>
           </MDBCarouselCaption>
         </div>
-      </MDBCarouselItem>
-    </MDBCarousel>
-  );
+      </MDBCarouselItem> */
+  }
 }
