0f96e0da852e32665f7ad6b33536708f68bfe736
JagadishBujji <jagadish00198@gmail.com>
2024-05-02 18:44:47 +0530

    color circle box-shadown

--- src/component/ProductDetails/ProductCard.js
+++ src/component/ProductDetails/ProductCard.js
@@ -300,6 +300,8 @@ const ProductCard = ({ product }) => {
                       height: "35px",
                       borderRadius: "50%",
                       backgroundColor: item.color_name,
+                      boxShadow:
+                        "rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px",
                       cursor: "pointer",
                       marginRight: "10px",
                       border:
